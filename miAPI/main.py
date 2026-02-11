8916304125678ad7a8fed30bc01ef62fcf706e87
NaooCG <123045508@upq.edu.mx>
2026-02-10 23:27:56 -0600

    Endspoints con parametros obligatorios/opcionales y  Swagger
    Endspoints con parametros obligatorios/opcionales y  Swagger

--- miAPI/main.py
+++ miAPI/main.py
@@ -17,3 +17,16 @@ async def bienvenido():
         "mensaje":"Hola mundo FastAPI",
         "estatus":"200"
         }
+#Endspoints con parametros obligatorios
+@app.get("/saludo/{nombre}")
+async def saludar(nombre: str):
+    return {"mensaje": f"Hola {nombre}"}
+
+#Endspoints con parametros opcionales
+from typing import Optional
+
+@app.get("/usuario/")
+async def obtener_usuario(edad: Optional[int] = None):
+    if edad:
+        return {"mensaje": f"Tienes {edad} años"}
+    return {"mensaje": "No enviaste edad"}
