bf669133e095b0f1d6459272cccaf8f4b4f24c5a
NaooCG <123045508@upq.edu.mx>
2026-03-24 15:03:50 -0600

    Practica No.9: SqlAquemy & PostgreSQL
    FINAL

--- miAPI/app/router/usuario.py
+++ miAPI/app/router/usuario.py
@@ -3,6 +3,10 @@ from app.models.usuario import crear_usuario
 from app.data.database import usuarios
 from app.security.auth import verificar_peticion
 
+from sqlalchemy.orm import Session
+from app.data.db import get_db
+from app.data.usuario import usuario as dbUsuario
+
 #simplifica no declarar cada endponit 
 router=APIRouter(
     prefix="/v1/usaurios",
@@ -11,25 +15,26 @@ router=APIRouter(
 
 #Endpoints
 @router.get("/")
-async def consulta():
+async def consulta(db:Session=Depends(get_db)): #aggrego
+
+    queryUsuarios=db.query(dbUsuario).all() #agrego
+
     return {
-        "total":len(usuarios),
-        "usuarios":usuarios,
+        "total":len(queryUsuarios), #remplazo usuarios por queryUsuarios
+        "usuarios":queryUsuarios, #remplazo usuarios por queryUsuarios
         "status":"200"
     }
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-async def agregar_usuario(usuario:crear_usuario): #SE USA EL MODELO
-    for usr in usuarios:
-        if usr["id"] == usuario.id: #cambia porque ya no usamos dict
-            raise HTTPException(
-                status_code= 400, 
-                detail="El id ya existe"
-                )
-    usuarios.append(usuario)
+async def agregar_usuario(usuarioP:crear_usuario,db:Session=Depends(get_db)):#agrego despues de la, db
+    nuevoU=dbUsuario(nombre=usuarioP.nombre, edad=usuarioP.edad)#agrego
+    db.add(nuevoU) #agrego
+    db.commit() #agrego
+    db.refresh(nuevoU) #agrego
+
     return {
         "mensaje":"usuario agregado",
-        "usuario":usuario 
+        "usuario":usuarioP 
     }
 
 @router.put("/{id}", status_code=status.HTTP_200_OK)
