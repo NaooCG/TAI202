d5e81b61b2d3580cf499d89be427aa3c005d3196
NaooCG <123045508@upq.edu.mx>
2026-03-25 00:51:32 -0600

    Practica No.10: SqlAlchemy Endpoints faltantes

--- miAPI/app/models/usuario.py
+++ miAPI/app/models/usuario.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, Field #agrega basemodel pydantic
+from typing import Optional
 
 #*********
 #modelo de validacion pydantic # se crea el modelo #agregamos validaciones perzonalizadas
@@ -6,3 +7,11 @@ from pydantic import BaseModel, Field #agrega basemodel pydantic
 class crear_usuario(BaseModel):
     nombre:str =Field(..., min_length=3,max_length=50,example="juanita")
     edad:int = Field (..., ge=1,le=123,description="edad valida entre 1 y 123")
+
+#*********
+# modelo para actualizar parcialmente
+#*********
+class UsuarioUpdate(BaseModel):
+    # Usamos Optional y None, pero mantenemos tus mismas validaciones
+    nombre: Optional[str] = Field(None, min_length=3, max_length=50, example="juanita")
+    edad: Optional[int] = Field(None, ge=1, le=123, description="edad valida entre 1 y 123")
